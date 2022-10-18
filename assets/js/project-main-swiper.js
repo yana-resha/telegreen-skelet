bf8269dd513af33d9498585371f9fa018bc3fa7a
yana-resha <79191411745@yandex.ru>
2022-10-18 13:12:25 +0300

    project ready

---
+++ assets/js/project-main-swiper.js
@@ -0,0 +1,47 @@
+var swiper = new Swiper(".project-main__swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 50,
+  slidesPerGroup: 3,
+  // loop: true,
+  // loopFillGroupWithBlank: true,
+  pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+  },
+  navigation: {
+  nextEl: ".project-main__swiper-button-next",
+  prevEl: ".project-main__swiper-button-prev",
+  },
+
+  breakpoints: {
+    1025: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+      slidesPerGroup: 3,
+    },
+
+    769: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+      slidesPerGroup: 2,
+  },
+
+  //    453: {
+  //      slidesPerView: 2,
+  //      spaceBetween: 34,
+  //      slidesPerGroup: 2,
+  //  },
+
+   577: {
+    slidesPerView: 2,
+    spaceBetween: 34,
+    slidesPerGroup: 2,
+},
+  1: {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    slidesPerGroup: 1,
+  }
+},
+
+});
---
+++ assets/js/project-part-swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".project-part__swiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  slidesPerGroup: 1,
+  // loop: true,
+  // loopFillGroupWithBlank: true,
+  pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+  },
+  
+})
